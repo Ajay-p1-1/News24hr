f46619814643da420332c28fc52a698b17902c6e
Ajay-p1-1 <ajaypadsala5682@gmail.com>
2024-07-19 21:31:10 +0530

    New project setup

---
+++ src/components/NewsItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+        This is NewsItem Component to crete news item.
+      </div>
+    )
+  }
+}
+
+export default NewsItem
